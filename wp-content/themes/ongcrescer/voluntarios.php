e49082fc86c5e6d4a8cfa698c4f38af19e0d6715
Wellington Santos <wjsantos.wj@gmail.com>
2017-12-02 23:16:43 -0200

    Estrutura da página de voluntarios

---
+++ wp-content/themes/ongcrescer/voluntarios.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Voluntários
+ *
+ * @package WordPress
+ * @subpackage OngCrescer
+ * @template voluntarios
+ * @since OngCrescer 2017-10-23
+ */
+?>
+
+<?php get_header(); ?>
+
+<main class="l-main">
+    <!-- conteudo da sua página -->
+    template voluntários
+</main>
+
+<?php  get_footer(); ?>
